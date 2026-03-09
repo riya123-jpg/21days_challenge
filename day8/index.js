6d98209c45cfe6d17ed7f2617a42b8bddd9a5126
riya gupta <griya3478@gmail.com>
2026-03-09 15:03:25 +0530

    message

---
+++ day8/index.js
@@ -0,0 +1,12 @@
+//day-8 javascript basic practice
+//store developer info
+let developerName = "Riya";
+let techStack = "HTML, CSS, JavaScript";
+//function to display developer info
+function showDeveloperInfo() {
+  console.log("Developer Name: " + developerName);
+  console.log("Current Tech Stack: " + techStack);
+  console.log("Learning JavaScript basics step by step");
+}
+//run the function
+showDeveloperInfo();
